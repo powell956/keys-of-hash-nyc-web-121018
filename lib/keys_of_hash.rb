8e721cee0878d85fc2f122ea40f5c405d587a49a
powell956 <powell956@gmail.com>
2018-12-31 07:37:43 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    keys = []
+
+    self.each do |key, value|
+      arguments.each do |el|
+        if value == el
+          keys << key
+        end
+      end
+    end
+
+    keys
   end
 end
